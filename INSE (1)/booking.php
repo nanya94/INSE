f1263257818b00ecb1328fd8bf721de1fac00771
nanya94 <akpansharon@yahoo.it>
2017-02-12 22:58:06 +0000

    Booking Website

---
+++ INSE (1)/booking.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<link href="booking.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300" rel="stylesheet">
+<head>
+  <title>
+    BOOKING
+  </title>
+</head>
+<body>
+  <header>
+    <div id="outer">
+      <a href="log-in.php">
+        <img src="img/UoPlogo.png" alt="Portsmouth University" id="vlogo"/>
+      </a>
+        <input type="submit" value="LOG OUT">
+      <div class="inner"><button type="submit" class="logout" onclick="return false;">Save</button></div>
+    </div>
+  </header>
+
+
+  </div>
+  <footer>
+    <p>All right reservered 2017</p>
+  </footer>
+</body>
+</html>
