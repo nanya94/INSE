eddb8a518bfe4a362902a392969a3d2111498d72
nanya94 <akpansharon@yahoo.it>
2017-02-13 21:56:08 +0000

    Add files via upload

--- INSE (1)/logout.php
+++ INSE (1)/logout.php
@@ -1,10 +1,11 @@
-<!doctype html><!DOCTYPE html>
+
+<!DOCTYPE html>
 <html>
 <link href="log-in_1.css" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300" rel="stylesheet">
 <head>
   <title>
-    RECOVER PASSWORD
+    LOGGED OUT
   </title>
 </head>
 <body>
@@ -15,22 +16,17 @@
       </a>
     </div>
   </header>
-  <h2 class="subtitle">Reset Password</h2>
+  <h2 class="subtitle">Logged Out from Session</h2>
   <p class="rvb-instc">
-    We can help you reset your password using your
-    University username or
-    the email address linked to your account.
+    You have sucesfully loged out from the
+      Library University Booking System.
+    </br>
+    You can now close window
   </p>
   <iframe src="about:blank" style="height: 0px; width: 0px; visibility: hidden; border: none; display: none;">
     This frame prevents back/forward cache problems in Safari.
   </iframe>
-  <div class="form">
-    <form method="post" action="sent.php">
-      <label for="uni-email"> Email or Username</label>
-      <input type="text" name="e-mail" id="email">
-      <input type="submit" value="Reset Password">
-    </form>
-  </div>
+
   <footer>
     <p>All right reservered 2017</p>
   </footer>
---
+++ INSE (1)/sent.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<link href="log-in_1.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300" rel="stylesheet">
+<head>
+  <title>
+    RECOVER PASSWORD
+  </title>
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <a href="log-in_1.php">
+        <img src="img/UoPlogo.png" alt="Portsmouth University"/>
+      </a>
+    </div>
+  </header>
+  
+  <body>
+  <h2 class="subtitle">Reset Password</h2>
+  <p class="rvb-instc">
+    If the email is linked with the account, an email has been sent to the
+      provided emal.
+    </br>
+    A people follow the instruction on the email.
+    </thead>
+  </p>
+  </body>
+
+  <footer>
+    <p>All right reservered 2017</p>
+  </footer>
+</body>
+</html>
