47fc2a8759461c4d2d34b09dc4b77a6bd743042f
nanya94 <akpansharon@yahoo.it>
2017-03-15 19:06:13 +0000

    Update register.php

--- INSE (1)/register.php
+++ INSE (1)/register.php
@@ -1,5 +1,4 @@
-
-<!DOCTYPE html>
+<!doctype html><!DOCTYPE html>
 <html>
 <link href="log-in_1.css" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300" rel="stylesheet">
@@ -11,26 +10,26 @@
 <body>
   <header>
     <div class="logo">
-      <a href="log-in_1.php">
+      <a href="log-in_1.html">
         <img src="img/UoPlogo.png" alt="Portsmouth University"/>
       </a>
     </div>
   </header>
   <div class="form">
-    <form method="post" action="made.php" id="register">
+    <form method="post" action="register.php" id="register">
         <h3 class="register"> Register </h3>
         <label for="name"> Your Full Name*: </label>
         <input type='text' name='name' id='name' maxlength="50" />
         <label for='email' >Email Address*:</label>
-        <input type='text' name='email' id='email' maxlength="50" />
+        <input type='email' name='email' id='email' maxlength="50" />
         <label for='username' >UserName*:</label>
-        <input type='text' name='username' id='username' maxlength="50" />
+        <input type='password' name='username' id='username' maxlength="50" />
         <label for='password' >Password*:</label>
         <div class="passowrd">
           <input type='password' name='password' id='password' maxlength="50" />
         </div>
         <input type='submit' name='Submit' value='Sign Up' class="_1on88 _7k49n" />
-        <label for ="legal-rights" id="legal"><a href="register.php"> By signing up, you agree to our Terms & Privacy Policy.</a></label>
+        <label for ="legal-rights" id="legal"><a href="register.html"> By signing up, you agree to our Terms & Privacy Policy.</a></label>
       </fieldset>
     </form>
     <div class="legal">
