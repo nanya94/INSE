098735c9f373a84df23cde1c646c78b82eae43fa
up785470 <up785470@myport.ac.uk>
2017-04-28 23:30:55 +0100

    Update Login.java
    Fixed the login system, so that checks on usernames and passswords actually function

--- Login.java
+++ Login.java
@@ -2,11 +2,13 @@ package org.example.aprilversion;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +16,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.Time;
 
 import static java.lang.Boolean.FALSE;
@@ -37,16 +41,12 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         data = new DatabaseCreater(this);
-        try{
 
-        }
-        finally{
-            data.close();
-        }
 
     }
 
     /**
+     *
      *
      * @param view - the current Activity
      * this method takes the user input from the username and password TextViews
@@ -54,51 +54,47 @@ public class Login extends AppCompatActivity {
      * if it returns true the user is returned to the next page
      * and the user is pushed through with putExtra
      */
-    public void login(View view){
+    public void login(View view) {
         Intent success = new Intent(this, Menu.class);
         EditText username = (EditText) findViewById(R.id.Username);
         EditText password = (EditText) findViewById(R.id.Password);
-        user = username.getText().toString();
+        user = username.getText().toString().toUpperCase();
         pass = password.getText().toString();
-        Cursor cursor = getStudents();
-        checkLoginDatabase(cursor, user, pass);
-        if (authorisation == TRUE)
+        Cursor cursor = checkStudent(user, pass);
+        checkLoginDatabase(cursor);
+        if (authorisation == TRUE) {
             success.putExtra(EXTRA_USER, user);
             startActivity(success);
-        } /**
+        }
+    }/**
             else {
-             Add fragment or text that says incorrect details
+              fragment says incorrect details
             */
 
 
     /**
+     * matches username and password against the database
      *
      * @param cursor
-     * @param username - username to be tested against the database
-     * @param password - password to be tested againast the database
-     *
-     * if a match for both the username and password is found, makes authorisation equal true.
-     * authorisation is set to false by default
      */
-    public void checkLoginDatabase(Cursor cursor, String username, String password){
-        while (cursor.moveToNext()) {
-            String user = cursor.getString(1);
-            String Pass = cursor.getString(4);
-            if ((username.toUpperCase().equals(user)) && (password.equals(pass))) {
+    public void checkLoginDatabase(Cursor cursor) {
+            if (cursor.moveToFirst()) {
                 authorisation = TRUE;
+            } else {
+                authorisation = FALSE;
+
             }
         }
-    }
+
 
 
     /**
+     * given the parameters adds a student to the database
      *
      * @param studentNum
      * @param firstName
      * @param lastName
      * @param password
-     *
-     * given the parameters adds a student to the database
      */
     private void addStudent(String studentNum, String firstName, String lastName, String password) {
         SQLiteDatabase db = data.getWritableDatabase();
@@ -113,46 +109,51 @@ public class Login extends AppCompatActivity {
 
 
 
-    private static String[] FROM = {STD_ID, STUDENT_NUM, FNAME, LNAME, PASSWORD};
-    private static String ORDER_BY = STUDENT_NUM + " DESC";
+
 
     /**
-     *
+     * runs a search query for a given student in the student table
+     * @param Password
+     * @param Username
      * @return returns a cursor for a search query for every student in the table STUDENT
      */
-    private Cursor getStudents() {
+    private Cursor checkStudent(String Username, String Password) {
         SQLiteDatabase db = data.getReadableDatabase();
-        Cursor cursor = db.query(STUDENT, FROM, null, null, null, null, ORDER_BY);
+        String[] FROM = {STUDENT_NUM, PASSWORD};
+        String WHERE = STUDENT_NUM + " = ?" + " AND " + PASSWORD + " = ?";
+        String[] CLAUSE = {Username, Password};
+        Cursor cursor = db.query(STUDENT, FROM, WHERE, CLAUSE, null, null, null);
         startManagingCursor(cursor);
         return cursor;
     }
 
     /**
+     * builds a string from the results of a search query
      *
-     * @param cursor for the cursor supplied from getStudents() builds a string with all the results
+     * @param cursor supplied from getStudents() query
      */
     public void showStudents(Cursor cursor){
         StringBuilder builder = new StringBuilder(
                 "All Students:\n");
         while (cursor.moveToNext()){
-            String id = cursor.getString(1);
-            String f = cursor.getString(2);
+            String id = cursor.getString(0);
+            String f = cursor.getString(1);/*
             String l = cursor.getString(3);
-            String p = cursor.getString(4);
+            String p = cursor.getString(4);*/
             builder.append(id).append(" - ");
             builder.append(f).append(" ");
-            builder.append(l).append(" - ");
-            builder.append(p).append("\n");
+/*            builder.append(l).append(" - ");
+            builder.append(p).append("\n");*/
         }
+
 
     }
 
     /**
+     * Adds a seat to the database
      *
      * @param floor - the floor the seat is on
      * @param isComp - represents a boolean, 1 for true 0 for false
-     *
-     * given the parameters adds a seat to the database
      */
     private void addSeat(int floor, int isComp){
         SQLiteDatabase db = data.getReadableDatabase();
@@ -166,6 +167,7 @@ public class Login extends AppCompatActivity {
     private static String ORDER_BY2 = FLOOR + " DESC";
 
     /**
+     * A query for searching the seat Table
      *
      * @return a cursor for a search query on the SEAT table
      */
@@ -197,13 +199,12 @@ public class Login extends AppCompatActivity {
 
 
     /**
+     * Adds a entry to the Time_slot table
      *
      * @param time
      * @param day
      * @param available
      * @param seat_ID
-     *
-     * given the parameters adds a Time_slot to the database
      */
     private void addTime(String time, String day, boolean available, int seat_ID){
         SQLiteDatabase db = data.getReadableDatabase();
@@ -214,9 +215,5 @@ public class Login extends AppCompatActivity {
         values.put(SEAT_NUM, seat_ID);
         db.insertOrThrow(TIME_SLOT, null, values);
     }
-
-
-
-
-
 }
+
